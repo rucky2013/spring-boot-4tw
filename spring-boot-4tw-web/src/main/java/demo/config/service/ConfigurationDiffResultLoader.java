6ced04af3857440af4b89bb6b47a4025f0f0e140
Stephane Nicoll <snicoll@pivotal.io>
2015-07-23 16:09:27 +0200

    Add caching of diffs

---
+++ spring-boot-4tw-web/src/main/java/demo/config/service/ConfigurationDiffResultLoader.java
@@ -0,0 +1,48 @@
+package demo.config.service;
+
+import java.io.IOException;
+
+import demo.config.diff.ConfigDiffGenerator;
+import demo.config.diff.ConfigDiffResult;
+import demo.config.diff.support.AetherDependencyResolver;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+/**
+ * Loads a diff between two Spring Boot versions.
+ *
+ * @author Stephane Nicoll
+ */
+@Service
+class ConfigurationDiffResultLoader {
+
+	private final ConfigDiffGenerator diffGenerator;
+
+	private final VersionStringComparator versionComparator;
+
+	public ConfigurationDiffResultLoader() {
+		this.diffGenerator = new ConfigDiffGenerator(AetherDependencyResolver.withAllRepositories());
+		this.versionComparator = new VersionStringComparator();
+	}
+
+	/**
+	 * Load the {@link ConfigDiffResult} between the {@code previousVersion} and the {@code nextVersion}.
+	 */
+	@Cacheable("diffs")
+	public ConfigDiffResult load(String previousVersion, String nextVersion) {
+		try {
+			Assert.hasText(previousVersion);
+			Assert.hasText(nextVersion);
+			if (versionComparator.compare(previousVersion, nextVersion) >= 0) {
+				throw new VersionMisMatchException(previousVersion, nextVersion);
+			}
+			return diffGenerator.generateDiff(previousVersion, nextVersion);
+		}
+		catch (IOException ex) {
+			throw new RepositoryNotReachableException(ex);
+		}
+	}
+
+}
--- spring-boot-4tw-web/src/main/java/demo/config/service/DiffMetadataService.java
+++ spring-boot-4tw-web/src/main/java/demo/config/service/DiffMetadataService.java
@@ -1,51 +1,28 @@
 package demo.config.service;
 
-import java.io.IOException;
-
-import demo.config.diff.ConfigDiffGenerator;
 import demo.config.diff.ConfigDiffResult;
-import demo.config.diff.support.AetherDependencyResolver;
 import demo.config.model.ConfigurationDiff;
 import demo.config.model.ConfigurationDiffHandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 @Service
 public class DiffMetadataService {
 
-	private final ConfigDiffGenerator diffGenerator;
+	private final ConfigurationDiffResultLoader diffResultLoader;
 
-	private final ConfigurationDiffHandler diffHandler;
+	private ConfigurationDiffHandler diffHandler;
 
-	private final VersionStringComparator versionComparator;
-
-	public DiffMetadataService() throws Exception {
-		this(new ConfigDiffGenerator(AetherDependencyResolver.withAllRepositories()),
-				new ConfigurationDiffHandler(),
-				new VersionStringComparator());
-	}
-
-	public DiffMetadataService(ConfigDiffGenerator diffGenerator, ConfigurationDiffHandler diffHandler,
-			VersionStringComparator versionComparator) {
-		this.diffGenerator = diffGenerator;
-		this.diffHandler = diffHandler;
-		this.versionComparator = versionComparator;
+	@Autowired
+	public DiffMetadataService(ConfigurationDiffResultLoader diffResultLoader) {
+		this.diffResultLoader = diffResultLoader;
+		this.diffHandler = new ConfigurationDiffHandler();
 	}
 
 	public ConfigurationDiff metadataDiff(String previousVersion, String nextVersion) {
-		try {
-			Assert.hasText(previousVersion);
-			Assert.hasText(nextVersion);
-			if (versionComparator.compare(previousVersion, nextVersion) >= 0) {
-				throw new VersionMisMatchException(previousVersion, nextVersion);
-			}
-			ConfigDiffResult result = diffGenerator.generateDiff(previousVersion, nextVersion);
-			return diffHandler.handle(result);
-		}
-		catch (IOException ex) {
-			throw new RepositoryNotReachableException(ex);
-		}
+		ConfigDiffResult result = this.diffResultLoader.load(previousVersion, nextVersion);
+		return diffHandler.handle(result);
 	}
 
 }
