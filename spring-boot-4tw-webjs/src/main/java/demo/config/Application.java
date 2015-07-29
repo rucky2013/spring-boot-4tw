43908a1adcfc1e0ad8a79dda830b524db6cfaf16
Stephane Nicoll <snicoll@pivotal.io>
2015-07-29 10:21:36 +0200

    Add health indicator

--- spring-boot-4tw-webjs/src/main/java/demo/config/Application.java
+++ spring-boot-4tw-webjs/src/main/java/demo/config/Application.java
@@ -1,8 +1,13 @@
 package demo.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
@@ -11,5 +16,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+	@Bean
+	public HealthIndicator springIoHealthIndicator() {
+		return () -> {
+			ResponseEntity<Object> entity = new RestTemplate()
+					.getForEntity("http://start.spring.io", Object.class);
+			return Health.up().withDetail("httpStatus", entity.getStatusCode()).build();
+		};
+	}
 
 }
