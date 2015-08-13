31424ca3b2e4545a200268d482a31989a008cb65
Stephane Nicoll <snicoll@pivotal.io>
2015-08-13 16:19:23 +0200

    Fix case

--- spring-boot-4tw-webjs/src/main/java/demo/config/service/VersionMisMatchException.java
+++
@@ -1,8 +0,0 @@
-package demo.config.service;
-
-public class VersionMismatchException extends RuntimeException {
-
-	public VersionMismatchException(String previousVersion, String nextVersion) {
-		super(previousVersion + " is not prior to " + nextVersion);
-	}
-}
