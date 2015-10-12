8e0bbe49cc2ef61ba336dd511c1de19e0b869561
Stephane Nicoll <snicoll@pivotal.io>
2015-10-12 14:04:13 +0200

    Actuator demonstration
    Once the actuator is in place, it is very easy to augment some of the
    endpoints to expose application-specific meta-data.
    
    The `info` endpoint exposes any arbitrary information about a running
    application. In this commit we expose the name and version (taken from
    the build information ) as well as git meta-data.
    
    Metrics can be easily recorded (both individual values and counters). In
    this commit, we record the number of times a particular version was
    requested. We also use the new `@ExportMetricWriter` annotation to
    export our metrics to a separate system. In this case, we simply expose
    them to JMX but plenty of other options are available (redis, statsd,
    etc).
    
    Finally, we could also update the `health` endpoint to expose a custom
    health indicator. For now, we're going to expose a simple one for
    demonstration purposes.

--- spring-boot-4tw-web/src/main/java/demo/config/Application.java
+++ spring-boot-4tw-web/src/main/java/demo/config/Application.java
@@ -1,10 +1,16 @@
 package demo.config;
 
+import java.util.Random;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
@@ -12,6 +18,7 @@ import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.jmx.export.MBeanExporter;
 
 @SpringBootApplication
 @EnableCaching
@@ -24,6 +31,26 @@ public class Application {
 	@Bean
 	public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
 		return container -> container.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html"));
+	}
+
+	@Bean
+	@ExportMetricWriter
+	public MetricWriter metricWriter(MBeanExporter exporter) {
+		return new JmxMetricWriter(exporter);
+	}
+
+	@Bean
+	public HealthIndicator forTheWebHealthIndicator() {
+		return () -> {
+			if (new Random().nextBoolean()) {
+				return Health.up().build();
+			}
+			else {
+				return Health.down()
+						.withDetail("Ooops", 42)
+						.build();
+			}
+		};
 	}
 
 	@Bean
